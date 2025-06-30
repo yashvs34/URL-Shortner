ea7b0718a5619408d02ff03b5c7fe9a060db9cd1
Harshvardhan Singh <yashvardhansingh232@gmail.com>
2025-06-30 22:33:15 +0530

    Modified logic for generating shortened id

--- backend/utils/generateShortId.js
+++ backend/utils/generateShortId.js
@@ -1,10 +1,32 @@
 
-let count = 0;
-
 function generateShortId ()
 {
-    let shortId = `${String.fromCharCode(97 + Math.floor(count / (26 * 26 * 26 * 26)) % 26)}${String.fromCharCode(97 + Math.floor(count / (26 * 26 * 26)) % 26)}${String.fromCharCode(97 + Math.floor(count / (26 * 26)) % 26)}${String.fromCharCode(97 + Math.floor(count / (26)) % 26)}${String.fromCharCode(97 + count % 26)}`;
-    count++;
+    let shortId = "";
+    let count1 = 4;
+    let count2 = 2;
+    let index1 = (Math.floor(10 * Math.random())) % 6;
+    let index2 = (Math.floor(10 * Math.random())) % 6;
+    
+    if (index1 == index2)
+    {
+        index2--;
+    }
+
+    let i = 0;
+
+    while (count1 || count2)
+    {
+        if (i == index1 || i == index2)
+        {
+            shortId += `${Math.floor(10 * Math.random()) % 10}`;
+            count2--;
+        }
+        else
+        {
+            shortId += `${String.fromCharCode(97 + Math.floor(100 * Math.random()) % 26)}`;
+            count1--;
+        }
+    }
 
     return shortId;
 }
