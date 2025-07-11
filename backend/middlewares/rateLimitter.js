93855bdd438c4fff1b4950426a2127a7928847b4
Harshvardhan Singh <yashvardhansingh232@gmail.com>
2025-07-11 15:42:02 +0530

    Configured number of requests to 10 requests/minute

--- backend/middlewares/rateLimitter.js
+++ backend/middlewares/rateLimitter.js
@@ -1,7 +1,7 @@
 const rateLimit = require('express-rate-limit')
 
 const limiter = rateLimit({
-    windowMs : 1 * 60 * 1000, // 3 requests/minute
+    windowMs : 1 * 60 * 1000, // 10 requests/minute
     limit : 10,
     message : "Too many requests from this IP, kindly try again after 1 minute",
     standardHeaders : true,
