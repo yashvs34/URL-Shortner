cbf8386b7d7212d5de877c733b2713e2d906bec7
yashvs34 <yashvardhansingh232@gmail.com>
2025-07-31 23:19:18 +0530

    Removed bug in rate-limitter

--- backend/middlewares/rateLimitter.js
+++ backend/middlewares/rateLimitter.js
@@ -6,14 +6,6 @@ const limiter = rateLimit({
     message : "Too many requests from this IP, kindly try again after 1 minute",
     standardHeaders : true,
     legacyHeaders : false,
-    skip : function (req, res) {
-        if (req.clientType && req.clientType === 'backend')
-        {
-            return true;
-        }
-
-        return false;
-    }
 })
 
 module.exports = limiter;
